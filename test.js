c7b28cf16ef06f9592b4794b1c49b0aed0314650
MichelWeber <michelpweber@gmail.com>
2018-05-07 16:15:21 -0700

    https used

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 alert("Do the ajax call here.");
-  $.ajax({url:"http://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
+  $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
          success: function(result){
           alert(result);
          }}
