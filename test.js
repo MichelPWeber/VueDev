922abd9c9f180ce90267157dd392ccd3fe60686d
MichelWeber <michelpweber@gmail.com>
2018-05-07 15:50:11 -0700

    added ajax call to state of Washington vessels

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
 alert("Do the ajax call here.");
+  $.ajax({url:"
+http://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
+         success: function(result){
+          alert(result);
+         }}
+  );
 });
