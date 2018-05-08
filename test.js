547586b4ced8d7ec65cb4f94f5c0be495483f97e
MichelWeber <michelpweber@gmail.com>
2018-05-08 08:51:18 -0700

    prints vessel names to console

--- test.js
+++ test.js
@@ -3,8 +3,9 @@ alert("Do the ajax call here.");
   $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
           dataType: "jsonp",
          success: function(result){
-           var obj = jQuery.parseJSON(result);
-          alert(obj);
+           $.each(result, function(key, value){
+             console.log(key + ": " + value.VesselName);
+           });
          }}
   );
 });
