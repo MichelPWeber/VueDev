7eb6b9125b97a7b89f6a5888a2d3d698e4cb1bfc
MichelWeber <michelpweber@gmail.com>
2018-05-09 16:00:08 -0700

    Removed alert statement

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-alert("Do the ajax call here.");
+
   $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
           dataType: "jsonp",
          success: function(result){
