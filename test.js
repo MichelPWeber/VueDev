00be4ba6a208dfa24517dc0feba37b3edb9b95bf
MichelWeber <michelpweber@gmail.com>
2018-05-11 08:00:24 -0700

    restructure

--- test.js
+++ test.js
@@ -1,5 +1,12 @@
-$(document).ready(function(){
+var app;
 
+$(document).ready(function(){
+  
+app = new Vue({
+  el: '#app',
+  data: {message: "Vue is working!"}
+});
+  
   $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
           dataType: "jsonp",
          success: function(result){
@@ -13,8 +20,3 @@ $(document).ready(function(){
 function jsonCallback(json){
   console.log(json);
 }
-
-var app = new Vue({
-  el: '#app',
-  data: {message: "Vue is working!"}
-});
