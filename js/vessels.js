32cb15431f78f8320e49e55e92a5e498f3f98827
MichelWeber <michelpweber@gmail.com>
2018-06-01 08:45:34 -0700

    created vessels.js

---
+++ js/vessels.js
@@ -0,0 +1,40 @@
+var app;
+var button;
+
+$(document).ready(function(){
+	
+Vue.component('vessel-item', {
+	template: "#grid-template",
+	props: { data: Array, columns: Array},
+	}
+);
+
+app = new Vue(
+{ 
+  el: '#app',
+  data:{  
+  message: "This will be a list of vessels.",
+  gridColumns: ['VesselName', 'Speed', 'Latitude','Longitude', 'ArrivingTerminalName'],
+  vessels:[]} 
+  }
+);
+	
+button = $("div button");
+button.on("click", function(){
+  GetData();
+});
+});
+
+
+function GetData(){
+  $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
+          dataType: "jsonp",
+         success: function(result){
+         app.vessels = result;
+         }}
+  );
+};
+
+function jsonCallback(json){
+  console.log(json);
+}
