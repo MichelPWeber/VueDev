4e7e413da2ec4d9f4db7356fbfd91648d1ffcb15
MichelWeber <michelpweber@gmail.com>
2018-06-07 16:19:02 -0700

    Added history of the last 10 vessel locations

--- js/vessels.js
+++ js/vessels.js
@@ -11,7 +11,7 @@ Vue.component('vessel-item', {
 
 Vue.component('graph-vessels', {
 		template: "#graph-template",
-		props: { data: Array},
+		props: { data: Array, history: Array},
 		computed: {
 			latCenter: function(){
 				var max = -999;
@@ -33,7 +33,37 @@ Vue.component('graph-vessels', {
 				});
 				
 				return (max+min)/2;
-			}
+			},
+			makeGrid: function(){
+				var i;
+				var width = 600;
+				var height = 600;
+				var spacing = width/10;
+				var gridSVG = "";
+				
+				for (i = 0; i <= width; i = i + spacing)
+				{
+					var x1 = i;
+					var y1 = 0;
+					var x2 = i;
+					var y2 = height;
+					var line = "<line x1='"+x1+"' y1='"+y1+"' x2='"+x2+"' y2='"+y2+"' style='stroke:rgb(196, 196, 196);stroke-width:.5'/>";
+					gridSVG = gridSVG + line;
+				}
+				
+				for (i = 0; i <= height; i = i + spacing)
+				{
+					var x1 = 0;
+					var y1 = i;
+					var x2 = width;
+					var y2 = i;
+					var line = "<line x1='"+x1+"' y1='"+y1+"' x2='"+x2+"' y2='"+y2+"' style='stroke:rgb(196, 196, 196);stroke-width:.5'/>";
+					gridSVG = gridSVG + line;
+				}
+				
+				return gridSVG;
+				//return "<rect x='50' y='50' width='50' height='50'></rect>";
+			},
 		}
 	}
 );
@@ -44,37 +74,52 @@ app = new Vue(
   data:{  
   message: "Vessel data: location.",
   gridColumns: ['VesselName', 'Speed', 'Latitude','Longitude', 'ArrivingTerminalName'],
+  vesselHistory: [],
   vessels:[]},
   	mounted(){
 		// set up timer
 		this.startTimer();
+		this.GetData();
 		}, 
   methods: {
 	  startTimer(){
-		  window.setInterval(this.timerTick, 1000*15);
+		  window.setInterval(this.timerTick, 1000*60);
 	  },
 	  timerTick(){
-		  GetData();
-	  }
-  },
-  }
-);
-	
-button = $("div button");
-button.on("click", function(){
-  GetData();
-});
-});
-
-
-function GetData(){
-  $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
+		  this.GetData();
+	  },
+	  GetData(){
+		  $.ajax({url:"https://www.wsdot.wa.gov/Ferries/API/Vessels/rest/vessellocations?apiaccesscode=7ff7eebd-711c-4126-830c-eab1aeb925c0",
           dataType: "jsonp",
          success: function(result){
-         app.vessels = result;
-         }}
-  );
-};
+			 app.UpdateHistory(app.vessels);
+			app.vessels = result;
+         }});
+	  },
+	  UpdateHistory(vesselData){
+		  $.each(vesselData, function(key, value){
+			//app.vesselHistory.push([value.VesselID, value.Latitude, value.Longitude]);  
+			if (app.NumberOfWithID(value.VesselID, app.vesselHistory) < 10)
+			{
+				app.vesselHistory.push({id:value.VesselID, lat:value.Latitude, lng:value.Longitude});
+			}
+		  });
+	  },
+	  NumberOfWithID(id, array)
+		{
+			var count = 0;
+			$.each(array, function(i, v){ if (v.VesselID === id) ++count;});
+			return count;
+		}
+	},
+  }
+);
+
+
+
+
+});
+
 
 function jsonCallback(json){
   console.log(json);
